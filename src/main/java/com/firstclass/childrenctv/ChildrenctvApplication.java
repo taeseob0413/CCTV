1632d0f6baf5a46f296d9fdeacbce6f17e79bd5b
ChangDaeJun <eowns1209@gmail.com>
2023-03-30 15:17:23 +0900

    create : 첫 프로젝트 생성

---
+++ src/main/java/com/firstclass/childrenctv/ChildrenctvApplication.java
@@ -0,0 +1,13 @@
+package com.firstclass.childrenctv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenctvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenctvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstclass/childrenctv/ChildrenctvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstclass.childrenctv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrenctvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
