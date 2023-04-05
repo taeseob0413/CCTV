e86ca6712e9e3c85fb07a970cf16a65f70a83533
0106ksh@naver.com <>
2023-04-05 14:24:52 +0900

    커밋

---
+++ test1.sql
@@ -0,0 +1,21 @@
+select * from tab;
+
+create table USER_table
+(
+  user_id long not null,
+  user_name varchar2(25),
+  user_email varchar2(50),
+  user_age number(5)
+);
+
+create table sale_table
+(
+  user_id long not null,
+  user_name varchar2(25),
+  user_email varchar2(50),
+  user_age number(5)
+);
+
+select * from USER_table;
+select * from USER_table;
+
