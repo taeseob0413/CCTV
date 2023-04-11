aff9203d86042d96af54b8841df5975bbaa9708b
Hoon <hyo04044@naver.com>
2023-04-11 16:56:56 +0900

    ReportBoardVO add Constructor

--- src/main/java/com/firstclass/childrenctv/domain/ReportBoardVO.java
+++ src/main/java/com/firstclass/childrenctv/domain/ReportBoardVO.java
@@ -13,5 +13,17 @@ public class ReportBoardVO {
 	private String report_content;		//제보 글 내용
 	private Long user_id;	// 사용자 고유번호
 	private Long child_id;	//실종아동 고유번호
+	
+	
+	public ReportBoardVO(Long report_id, Date report_time, String report_address, String report_content, Long user_id, Long child_id) {
+		this.report_id = report_id;
+		this.report_time = report_time;
+		this.report_address = report_address;
+		this.report_content = report_content;
+		this.user_id = user_id;
+		this.child_id = child_id;
+	}
 
+	
+	
 }
