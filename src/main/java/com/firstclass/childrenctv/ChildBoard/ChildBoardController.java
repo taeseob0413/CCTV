6369ed73bbbb8759ed70b7da5bf90725d463be52
charley413 <charley0715@naver.com>
2023-04-13 11:47:32 +0900

    child board 서비스

---
+++ src/main/java/com/firstclass/childrenctv/ChildBoard/ChildBoardController.java
@@ -0,0 +1,25 @@
+package com.firstclass.childrenctv.ChildBoard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/child/*")
+@AllArgsConstructor
+public class ChildBoardController {
+	private ChildBoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		model.addAttribute("list",service.getChild());
+	}
+	@GetMapping("/get")
+	public void get(@RequestParam("child_id") Long child_id, Model model) {
+		model.addAttribute("board",service.getChildId(child_id));
+	}
+}
--- src/main/java/com/firstclass/childrenctv/ChildBoard/ChildBoardServiceImpl.java
+++ src/main/java/com/firstclass/childrenctv/ChildBoard/ChildBoardServiceImpl.java
@@ -2,30 +2,40 @@ package com.firstclass.childrenctv.ChildBoard;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ChildBoardServiceImpl implements ChildBoardService {
+   
+   @Autowired
+   private ChildBoardMapper mapper;
 
-	@Override
-	public void insert() {
-		// TODO Auto-generated method stub
+   @Override
+   public List<ChildBoardVO> getChildAddr(String addr) {
+	   return mapper.getChildAddr(addr);
 
-	}
+   }
 
-	@Override
-	public List<ChildBoardVO> getChild() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   public List<ChildBoardVO> getChild() {
+      // TODO Auto-generated method stub
+      return mapper.getChild();
+   }
 
-	@Override
-	public ChildBoardVO getChildId(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   public ChildBoardVO getChildId(Long id) {
+      // TODO Auto-generated method stub
+      return mapper.getChildId(id);
+   }
 
-	@Override
-	public void insert(ChildBoardVO child) {
-		// TODO Auto-generated method stub
+   @Override
+   public void insert(ChildBoardVO child) {
+      mapper.insert(child);
 
-	}
+   }
 
 }
