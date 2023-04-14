d787772e6b9dfd03d9540e776b27246b6a678d29
Lynn <romanstruck@naver.com>
2023-04-14 11:32:26 +0900

    Merge branch 'sts' of https://github.com/ChangDaeJun/CCTV.git into pji

--- src/main/java/com/firstclass/childrenctv/ChildBoard/ChildBoardController.java
+++ src/main/java/com/firstclass/childrenctv/ChildBoard/ChildBoardController.java
@@ -19,10 +19,15 @@ public class ChildBoardController {
 	
 	@GetMapping("/child/list")
 	public String list(Model model) {
-		String child_location="송파";
+		String child_location="seoul";
 		model.addAttribute("list", service.getChildAddr(child_location));
 		System.out.println("성공!");
 		return "/childboard/child";
 	}
+	
+	@GetMapping("/child/get")
+	public void get(@RequestParam("child_id") Long child_id, Model model) {
+		model.addAttribute("board",service.getChildId(child_id));
+	} 
 
 }
