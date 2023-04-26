e2e3d83a2f094b715e2f2d01b1b4f22939812030
Hoon <hyo04044@naver.com>
2023-04-26 16:05:42 +0900

    bug fix : " deleted childboard, childapi restore"

---
+++ src/main/java/com/firstclass/childrenctv/childBoard/ChildBoardService.java
@@ -0,0 +1,21 @@
+package com.firstclass.childrenctv.childBoard;
+
+import java.util.List;
+
+
+import com.firstclass.childrenctv.util.paging.Criteria;
+
+public interface ChildBoardService {
+	   List<ChildBoardVO> getChildAddr(String child_location);
+	   
+	   List<ChildBoardVO> getChild();
+	   
+	   ChildBoardVO getChildId(long id);
+	   
+	   List<ChildBoardVO> getChildList(Criteria cri);
+	   
+	   void insert(List<ChildBoardVO> childList);
+	   
+	   public int getTotal(Criteria cri);
+
+}
---
+++ src/main/java/com/firstclass/childrenctv/childBoard/ChildBoardServiceImpl.java
@@ -0,0 +1,59 @@
+package com.firstclass.childrenctv.childBoard;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.firstclass.childrenctv.util.paging.Criteria;
+
+import lombok.AllArgsConstructor;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+@Service
+@AllArgsConstructor
+public class ChildBoardServiceImpl implements ChildBoardService {
+   
+   @Autowired
+   private ChildBoardMapper mapper;
+
+   @Override
+   public List<ChildBoardVO> getChildAddr(String child_location) {
+      return mapper.getChildAddr(child_location);
+
+   }
+
+   @Override
+   public List<ChildBoardVO> getChild() {
+      // TODO Auto-generated method stub
+      return mapper.getChild();
+   }
+
+   @Override
+   public ChildBoardVO getChildId(long id) {
+      // TODO Auto-generated method stub
+      return mapper.getChildId(id);
+   }
+
+   @Override
+   public void insert(List<ChildBoardVO> childList) {
+      for(ChildBoardVO entity:childList) {
+         mapper.insert(entity);
+      }
+   }
+
+   @Override
+   public List<ChildBoardVO> getChildList(Criteria cri) {
+      return mapper.getChildWithPaging(cri);
+   
+   }
+
+   @Override
+   public int getTotal(Criteria cri) {
+      return mapper.getTotal(cri);
+   }
+
+}
