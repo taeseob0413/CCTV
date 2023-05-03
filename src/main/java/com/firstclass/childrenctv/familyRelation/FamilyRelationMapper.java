12676b276b7e7678dca9e0f65e7cddff2f14eb36
장대준 <jangdaejun@jangdaejun-ui-MacBookPro.local>
2023-05-03 16:25:36 +0900

    feat : 제보 알림 신청 버튼 추가

--- src/main/java/com/firstclass/childrenctv/familyRelation/FamilyRelationMapper.java
+++ src/main/java/com/firstclass/childrenctv/familyRelation/FamilyRelationMapper.java
@@ -9,6 +9,7 @@ public interface FamilyRelationMapper {
     void save(FamilyRelationVO relation);
     List<FamilyRelationVO> findRelationByChild(Long child_id);
     List<FamilyRelationVO> findRelationByUser(Long user_id);
+    List<FamilyRelationVO> findRelationByUserAndChild(Long user_id, Long child_id);
     void approval(Long id);
     void delete(Long id);
     
